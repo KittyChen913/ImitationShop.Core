176ea7030f28acd97c0994fa91b2f982167dc795
KittyChen913 <nightmare.kitty111@gmail.com>
2022-05-01 13:18:01 +0800

    ✨ add EF Core DbContext

---
+++ ImitationShop.EFCore/Context/ImitationShopDBContext.cs
@@ -0,0 +1,45 @@
+﻿using ImitationShop.EFCore.DbModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace ImitationShop.EFCore.Context;
+
+public partial class ImitationShopDBContext : DbContext
+{
+    private readonly IConfiguration configuration;
+
+    public ImitationShopDBContext(IConfiguration configuration)
+    {
+        this.configuration = configuration;
+    }
+
+    public ImitationShopDBContext(IConfiguration configuration, DbContextOptions<ImitationShopDBContext> options)
+        : base(options)
+    {
+        this.configuration = configuration;
+    }
+
+    public virtual DbSet<Item> Items { get; set; } = null!;
+
+    // 連接數據庫時要做的一些配置
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(configuration.GetConnectionString("ImitationShopDB"));
+    }
+
+    // 配置實體跟數據庫彼此之間的映射關係
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Item>(entity =>
+        {
+            entity.Property(e => e.ItemName).HasMaxLength(50);
+
+            entity.Property(e => e.ItemPrice).HasColumnType("decimal(18, 0)");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    // 需要額外添加自定義的處理
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ ImitationShop.EFCore/DbModels/Item.cs
@@ -0,0 +1,9 @@
+﻿namespace ImitationShop.EFCore.DbModels
+{
+    public partial class Item
+    {
+        public int ItemId { get; set; }
+        public string ItemName { get; set; } = null!;
+        public decimal? ItemPrice { get; set; }
+    }
+}
